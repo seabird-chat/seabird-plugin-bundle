26177bd40c7a030ab869ceffe4fd3e323215d7b4
Kaleb Elwert <belak@coded.io>
2020-02-01 12:13:59 -0800

    Initial connection

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,83 @@
+use std::net::ToSocketAddrs;
+
+use native_tls::TlsConnector;
+use tokio::io::{AsyncRead, AsyncWrite, ReadHalf, WriteHalf, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio::stream::StreamExt;
 use tokio::sync::mpsc;
-use tokio::time::{delay_for, Duration};
+use tokio_tls::TlsStream;
+use tokio_util::codec::FramedRead;
 
-struct Message {
-    command: String,
-}
+mod codec;
+use codec::IrcCodec;
 
-impl<'a> From<irc::Message<'a>> for Message {
-    fn from(msg: irc::Message<'a>) -> Self {
-        Message {
-            command: msg.command.to_string(),
-        }
+async fn read_task<T>(reader: T, out: mpsc::Sender<String>) -> Result<(), anyhow::Error>
+where
+    T: AsyncRead + Unpin,
+{
+    let mut framed = FramedRead::new(reader, IrcCodec::new());
+
+    while let Some(msg) = framed.next().await.transpose()? {
+        println!("<-- {}", msg);
     }
-}
 
-async fn read_task() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-async fn send_task(mut msgs: mpsc::Receiver<String>) -> Result<(), anyhow::Error> {
-    loop {
-        delay_for(Duration::from_secs(1)).await;
-        match msgs.recv().await {
-            Some(line) => println!("GOT LINE: {}", line),
-            None => anyhow::bail!("Dead send queue"),
-        }
+async fn send_task<T>(mut writer: T, mut msgs: mpsc::Receiver<String>) -> Result<(), anyhow::Error>
+where
+    T: AsyncWrite + Unpin,
+{
+    while let Some(line) = msgs.recv().await {
+        println!("--> {}", line);
+        writer.write_all(line.as_bytes()).await?;
+        writer.write_all(b"\r\n").await?;
     }
+
+    // TODO: this is actually an error - the send queue dried up.
+    Ok(())
+}
+
+async fn connect(
+    target: &str,
+) -> Result<
+    (
+        ReadHalf<TlsStream<TcpStream>>,
+        WriteHalf<TlsStream<TcpStream>>,
+    ),
+    anyhow::Error,
+> {
+    let addr = target
+        .to_socket_addrs()?
+        .next()
+        .ok_or(anyhow::anyhow!("Failed to look up address"))?;
+
+    let socket = TcpStream::connect(&addr).await?;
+    let cx = TlsConnector::builder()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+    let cx = tokio_tls::TlsConnector::from(cx);
+
+    let socket = cx.connect(target, socket).await?;
+
+    let (reader, writer) = tokio::io::split(socket);
+
+    Ok((reader, writer))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    let (reader, writer) = connect("chat.freenode.net:6697").await?;
+
     let (mut tx_send, rx_send) = mpsc::channel(100);
 
-    let read = tokio::spawn(read_task());
-    let send = tokio::spawn(send_task(rx_send));
+    let read = tokio::spawn(read_task(reader, tx_send.clone()));
+    let send = tokio::spawn(send_task(writer, rx_send));
 
     tx_send.send("NICK seabird51".to_string()).await?;
-    tx_send.send(format!("USER seabird 0.0.0.0 0.0.0.0 :Seabird Bot")).await?;
+    tx_send
+        .send(format!("USER seabird 0.0.0.0 0.0.0.0 :Seabird Bot"))
+        .await?;
 
     match tokio::try_join!(read, send) {
         Err(e) => {
