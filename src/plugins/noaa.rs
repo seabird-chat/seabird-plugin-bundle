1a5dbe3d820d22dc64aa2a0d02a4978112e3fc00
Kaleb Elwert <belak@coded.io>
2020-02-05 02:43:52 -0800

    noaa: initial commit
    The initial version contains only the metar command.

---
+++ src/plugins/noaa.rs
@@ -0,0 +1,51 @@
+use std::io::BufRead;
+
+use async_trait::async_trait;
+
+use crate::prelude::*;
+
+pub struct NoaaPlugin {
+    base_url: &'static str,
+}
+
+impl NoaaPlugin {
+    pub fn new() -> Self {
+        NoaaPlugin {
+            base_url: "https://tgftp.nws.noaa.gov/data",
+        }
+    }
+}
+
+#[async_trait]
+impl Plugin for NoaaPlugin {
+    async fn handle_message(&self, ctx: &Context) -> Result<()> {
+        match ctx.as_event() {
+            Event::Command("metar", Some(station)) => {
+                let mut station = station.to_string();
+                station.make_ascii_uppercase();
+
+                let data = reqwest::get(
+                    &format!(
+                        "{}/observations/metar/stations/{}.TXT",
+                        self.base_url, station
+                    )[..],
+                )
+                .await?
+                .error_for_status()?
+                .text()
+                .await?;
+
+                // Because the first line is the date, we need to skip it.
+                let mut lines = std::io::Cursor::new(data).lines();
+                let _ = lines.next();
+                let line = lines.next().transpose()?.ok_or_else(|| anyhow::anyhow!("No results"))?;
+
+                ctx.mention_reply(&line[..]).await?;
+            }
+            Event::Command("metar", None) => {}
+            _ => {}
+        }
+
+        Ok(())
+    }
+}
