b487671430842310dbd0cac4902464e5924f488b
jsvana <jaysvana@gmail.com>
2020-02-07 08:52:13 -0800

    Add taf to noaa plugin
    Adds <prefix>taf to get the TAF for a station

--- src/plugins/noaa.rs
+++ src/plugins/noaa.rs
@@ -19,6 +19,19 @@ impl NoaaPlugin {
     }
 }
 
+async fn lines_from_url(url: &str) -> Result<std::io::Lines<std::io::Cursor<String>>> {
+    let start = Instant::now();
+    let data = reqwest::get(url).await?.error_for_status()?.text().await?;
+
+    trace!(
+        "Got station information from \"{}\" in {}ms",
+        url,
+        start.elapsed().as_millis()
+    );
+
+    Ok(std::io::Cursor::new(data).lines())
+}
+
 #[async_trait]
 impl Plugin for NoaaPlugin {
     async fn handle_message(&self, ctx: &Arc<Context>) -> Result<()> {
@@ -27,26 +40,11 @@ impl Plugin for NoaaPlugin {
                 let mut station = station.to_string();
                 station.make_ascii_uppercase();
 
-                let start = Instant::now();
-                let data = reqwest::get(
-                    &format!(
-                        "{}/observations/metar/stations/{}.TXT",
-                        self.base_url, station
-                    )[..],
-                )
-                .await?
-                .error_for_status()?
-                .text()
+                let mut lines = lines_from_url(&format!(
+                    "{}/observations/metar/stations/{}.TXT",
+                    self.base_url, station
+                ))
                 .await?;
-
-                trace!(
-                    "Got station information for {} in {}ms",
-                    station,
-                    start.elapsed().as_millis()
-                );
-
-                // Because the first line is the date, we need to skip it.
-                let mut lines = std::io::Cursor::new(data).lines();
                 let _ = lines.next();
                 let line = lines
                     .next()
@@ -55,7 +53,39 @@ impl Plugin for NoaaPlugin {
 
                 ctx.mention_reply(&line[..]).await?;
             }
-            Event::Command("metar", None) => {}
+            // TODO: implement stored stations
+            Event::Command("metar", None) => {
+                ctx.mention_reply(&format!(
+                    "Missing station argument. Usage: {}metar <station>",
+                    ctx.command_prefix()
+                ))
+                .await?;
+            }
+            Event::Command("taf", Some(station)) => {
+                let mut station = station.to_string();
+                station.make_ascii_uppercase();
+
+                let mut lines = lines_from_url(&format!(
+                    "{}/forecasts/taf/stations/{}.TXT",
+                    self.base_url, station
+                ))
+                .await?;
+                let _ = lines.next();
+
+                for line in lines {
+                    if let Ok(line) = line {
+                        ctx.mention_reply(&line.trim()).await?;
+                    }
+                }
+            }
+            // TODO: implement stored stations
+            Event::Command("taf", None) => {
+                ctx.mention_reply(&format!(
+                    "Missing station argument. Usage: {}taf <station>",
+                    ctx.command_prefix()
+                ))
+                .await?;
+            }
             _ => {}
         }
 
