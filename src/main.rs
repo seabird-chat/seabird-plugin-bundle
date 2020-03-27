ab56d0c8799feedee6a97b574610be6289ceaebc
Kaleb Elwert <belak@coded.io>
2020-03-27 10:46:22 -0700

    main: default to more verbose logging

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,21 @@ where
 
 #[tokio::main]
 async fn main() -> error::Result<()> {
+    // Try to load dotenv before loading the logger or trying to set defaults.
+    let env_res = dotenv::dotenv();
+
+    // There's a little bit of an oddity here, since we want to set it if it
+    // hasn't already been set, but we want this done before the logger is loaded.
+    if let Err(_) = std::env::var("RUST_LOG") {
+        std::env::set_var("RUST_LOG", "info,seabird=debug");
+    }
+
+    // Now that everything is set up, load up the logger.
     pretty_env_logger::init_timed();
 
     // We ignore failures here because we want to fall back to loading from the
     // environment.
-    if let Ok(path) = dotenv::dotenv() {
+    if let Ok(path) = env_res {
         info!("Loaded env from {:?}", path);
     }
 
